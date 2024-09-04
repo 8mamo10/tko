65643ec425581a8d29ec57f8eb8ea7c2eae4fb73
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-09-04 07:11:39 +0700

    Add logic to check the behavior of await

--- tko/src/main.rs
+++ tko/src/main.rs
@@ -1,7 +1,15 @@
 use mini_redis::{client, Result};
 
+async fn say_world() {
+    println!("world");
+}
+
 #[tokio::main]
 pub async fn main() -> Result<()> {
+    let op = say_world();
+    println!("hello");
+    op.await;
+
     let mut client = client::connect("127.0.0.1:6379").await?;
     client.set("hello", "world".into()).await?;
     let result = client.get("hello").await?;
