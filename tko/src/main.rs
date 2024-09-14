5ebd2f56b106954e331d726d681b34088dd03023
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-09-14 16:59:48 +0700

    Make the database unique

--- tko/src/main.rs
+++ tko/src/main.rs
@@ -1,31 +1,30 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type DB = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
-    //let handle = tokio::spawn(async { "return value" });
-
     let listner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::<String, Bytes>::new()));
 
     loop {
         let (socket, _) = listner.accept().await.unwrap();
+        let db = db.clone();
+
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
-
-    //let out = handle.await.unwrap();
-    //println!("GOT {}", out);
 }
 
-/// The function `process` asynchronously reads a frame from a TCP stream, prints it, and then writes an
-/// error response back to the stream.
-///
-/// Arguments:
-///
-/// * `socket`: The `socket` parameter in the `process` function is of type `TcpStream`, which
-/// represents a TCP stream between the client and server for network communication.
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: DB) {
     use mini_redis::Command::{self, Get, Set};
     use std::collections::HashMap;
 
