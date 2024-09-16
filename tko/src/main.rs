df4767bafcbeec611e15029adceec6da6488ee1d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-09-16 12:55:13 +0700

    Use the single database in process

--- tko/src/main.rs
+++ tko/src/main.rs
@@ -26,21 +26,20 @@ async fn main() {
 
 async fn process(socket: TcpStream, db: DB) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
