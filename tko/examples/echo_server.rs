314ef769a86298aa4d182c418485cf7e2128a552
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-10-31 23:54:16 +0700

    Update echo server

--- tko/examples/echo_server.rs
+++ tko/examples/echo_server.rs
@@ -1,4 +1,4 @@
-use tokio::io;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -6,7 +6,23 @@ async fn main() -> io::Result<()> {
     //let mut listner = TcpListener::bind("127.0.0.1:6142").await.unwrap();
     let listner = TcpListener::bind("127.0.0.1:6142").await.unwrap();
     loop {
-        let (mut _socket, _) = listner.accept().await?;
-        tokio::spawn(async move {});
+        let (mut socket, _) = listner.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return,
+                    Ok(n) => {
+                        println!("Received: {}", n);
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+        });
     }
 }
