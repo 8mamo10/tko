38b015c30c457002f6e56653999600a309a089f5
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-11-06 23:55:47 +0700

    Add constructor for connection

--- tko/src/connection.rs
+++ tko/src/connection.rs
@@ -1,11 +1,19 @@
+use bytes::ByteMut;
 use mini_redis::{Frame, Result};
 use tokio::net::TcpStream;
 
 struct Connection {
     stream: TcpStream,
+    buffer: ByteMut,
 }
 
 impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            buffer: ByteMut::with_capacity(4096),
+        }
+    }
     pub async fn read_frame(&mut self) -> Result<Option<Frame>> {}
     pub async fn write_frame(&mut self, frame: &Frame) -> Result {}
 }
