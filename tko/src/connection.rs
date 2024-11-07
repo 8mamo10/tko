5d9983fd3ff57ca7a2c300f342a58a2bb46cd87c
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-11-07 12:02:41 +0700

    Implement read_frame

--- tko/src/connection.rs
+++ tko/src/connection.rs
@@ -14,6 +14,17 @@ impl Connection {
             buffer: ByteMut::with_capacity(4096),
         }
     }
-    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {}
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        if let Some(frame) = self.parse_frame()? {
+            return Ok(Some(frame));
+        }
+        if 0 == self.stream.read_buf(&mut self.buffer).await? {
+            if self.buffer.is_empty() {
+                return Ok(None);
+            } else {
+                return Err("connection reset by peer".into());
+            }
+        }
+    }
     pub async fn write_frame(&mut self, frame: &Frame) -> Result {}
 }
